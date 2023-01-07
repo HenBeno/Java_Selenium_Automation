85e9b25b9ae29747100a3a1152391262a21df7ca
Hen Benoish <henbenhen@gmail.com>
2023-01-07 11:33:13 +0200

    Remove unnecessary page from PageObject

--- src/main/java/test/java/Utilities/PageObjectManager.java
+++ src/main/java/test/java/Utilities/PageObjectManager.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.support.PageFactory;
 public class PageObjectManager extends Base {
     public static void initWebPage() {
 
-        topMenuBarPage = PageFactory.initElements(driver, TopMenuBarPage.class);
-//        PageFactory.initElements(new FieldContextDecorator(new ElementContextLocatorFactory(driver)), TopMenuBarPage.class);
         loginPageFB = PageFactory.initElements(driver, LoginPageFB.class);
         fbMainPage = PageFactory.initElements(driver, FBMainPage.class);
         fbTopMenu = PageFactory.initElements(driver, FBTopMenu.class);
